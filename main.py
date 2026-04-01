5b632b826f0e1afc5fa0ccf1278485ba602fcb00
rohi-nagedram <bathinirohini61@gmail.com>
2026-04-01 08:38:36 +0530

    Fix typo in signed transaction property

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def register_on_chain(agent_name: str) -> str:
         })
 
         signed = w3.eth.account.sign_transaction(tx, PRIVATE_KEY)
-        tx_hash = w3.eth.send_raw_transaction(signed.raw_transaction)
+        tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
 
         return f"https://sepolia.basescan.org/tx/{tx_hash.hex()}"
 
